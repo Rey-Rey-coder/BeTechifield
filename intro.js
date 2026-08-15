c3700a8604a80c06da0e590b58cd5d3fa8ff4818
Rey-Rey-coder <allahuliudati77@gmail.com>
2026-08-15 21:45:39 +0100

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,12 @@
+// intro.js
+// This file logs some basic information about me to the console
+
+const name = "Babatunde Ilyas Olanrewaju";
+const age = 20;
+const favoriteLanguage = "JavaScript & PHP";
+const BeTechifielduniquenumber = "BD26080534"
+
+console.log("Name: " + name);
+console.log("Age: " + age);
+console.log("My Favorite Programming Language: " + favoriteLanguage);
+console.log("My BeTechifield unique number is: " + BeTechifielduniquenumber);
